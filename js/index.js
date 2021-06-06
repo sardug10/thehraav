8a999175a99b50f4f36299598637f588c49585c4
Sarthak Duggal <duggal.sarthak12@gmail.com>
2021-06-07 02:12:12 +0530

    Added Blog

--- js/index.js
+++ js/index.js
@@ -6,3 +6,21 @@ menuButton.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
   navBar.classList.toggle("bigger-nav");
 });
+
+//JAVASCRIPT FOR CAROUSEL
+
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  cellAlign: "left",
+  containe: true,
+  autoPlay: 1500,
+  pageDots: false,
+  wrapAround: true,
+  pauseAutoPlayOnHover: true,
+  imagesLoaded: true,
+  prevNextButtons: false,
+});
+
+elem.addEventListener("mouseleave", () => {
+  flkty.playPlayer();
+});
